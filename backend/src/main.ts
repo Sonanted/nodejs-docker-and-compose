2c0ab116f4bddc8a03aee352e8923d888f87bbbc
Andrey Nikonenko <sonanted@gmail.com>
2026-03-23 11:33:32 +0300

    fix: backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: ['https://sonanted.nomorepartiessite.ru'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(8000);
 }
 bootstrap();
